7b60d6e5bbff8847f1e5d9788b543c56b1424db0
ellisvelandia <eyis619@gmail.com>
2022-09-19 14:10:41 -0500

    express

---
+++ .history/index_20220919140615.js
@@ -0,0 +1,14 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+A
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
+
+
+
